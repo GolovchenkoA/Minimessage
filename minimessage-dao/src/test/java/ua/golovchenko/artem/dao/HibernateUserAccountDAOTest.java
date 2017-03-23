144ffb326235d188c2507dae9d8b6aac4c61d4a1
Golovchenklo Artem <Golovchenklo84@mail.ru>
2017-03-23 16:24:50 +0200

    Before move package ua.golovchenko.artem.dao to ua.golovchenko.artem.minimessages.dao

---
+++ minimessage-dao/src/test/java/ua/golovchenko/artem/dao/HibernateUserAccountDAOTest.java
@@ -0,0 +1,43 @@
+package ua.golovchenko.artem.dao;
+
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(locations = {"classpath:persistence-context-dev.xml"})
+public class HibernateUserAccountDAOTest {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+
+    }
+}
